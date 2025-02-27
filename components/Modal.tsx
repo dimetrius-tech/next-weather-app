b4ae2d7a873518605c1fecce5add6c0972f1b7fe
coding <coding.io@proton.me>
2025-02-28 00:01:29 +0200

    remove logic to build prod

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import Link from "next/link";
-import {useActionState} from "react";
-import {authenticate} from "@/lib/auth";
 
 export default function Modal() {
-    const [errorMessage, formAction, isPending] = useActionState(authenticate, undefined);
     return (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
             <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
@@ -14,7 +11,7 @@ export default function Modal() {
                     <div className="flex items-center justify-center min-h-screen bg-gray-100">
                         <div className="bg-white p-6 rounded-lg shadow-md w-80">
                             <h2 className="text-xl font-semibold text-center mb-4">Login</h2>
-                            <form action={formAction} className="space-y-4">
+                            <form className="space-y-4">
                                 <input
                                     id="email"
                                     name="email"
@@ -40,12 +37,10 @@ export default function Modal() {
                         {/* Navigates back to the base URL - closing the modal */}
                         <Link
                             href="/"
-                            aria-disabled={isPending}
                             className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
                         >
                             Close
                         </Link>
-                        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
                     </div>
                 </div>
             </div>
